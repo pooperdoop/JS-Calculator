4fb1ef4fbd62b7f4ce96aef6ec6200164f0a2443
Bustah <psnatb2lme@gmail.com>
2024-12-19 09:00:58 +0800

    code encapsulation
    thx Etan31

--- script.js
+++ script.js
@@ -1,79 +1,44 @@
-var calculation;
-var periodCheck = false;
-var ParCheck = false;
-var parCount = 0;
+var calculation; // saves value of field when needed
+var periodCheck = false; // checks if period exists within a number
+var ParCheck = false; // checks whether open or close parenthesis should be placed
+var parCount = 0; // number of open parenthesis within the equation
 
 
 
 
-function EnterNum(n){
+function EnterNum(input){
     CheckForPar();
     Cleanup();
 
-    switch (n){
-        case 0:
-            document.getElementById('enterfield').value += '0'; 
-            ParCheck = true; 
-            break;
-        case 1:
-            document.getElementById('enterfield').value += '1'; 
-            ParCheck = true; 
-            break;
-        case 2:
-            document.getElementById('enterfield').value += '2'; 
-            ParCheck = true; 
-            break;
-        case 3:
-            document.getElementById('enterfield').value += '3'; 
-            ParCheck = true; 
-            break;
-        case 4:
-            document.getElementById('enterfield').value +='4'; 
-            ParCheck = true; 
-            break;
-        case 5:
-            document.getElementById('enterfield').value +='5'; 
-            ParCheck = true; 
-            break;
-        case 6:
-            document.getElementById('enterfield').value +='6'; 
-            ParCheck = true; 
-            break;
-        case 7:
-            document.getElementById('enterfield').value +='7'; 
-            ParCheck = true; 
-            break;
-        case 8:
-            document.getElementById('enterfield').value +='8'; 
-            ParCheck = true; 
-            break;
-        case 9:
-            document.getElementById('enterfield').value +='9'; 
-            ParCheck = true; 
-            break;
-        case 'P':
-            if(ParCheck == false){
-                document.getElementById('enterfield').value +='('; 
-                parCount++;
-            } else{
-                document.getElementById('enterfield').value +=')'; 
-                parCount--;
-            }
-            break;
-        case '.':
-            if(periodCheck == false){
-             document.getElementById('enterfield').value +='.'; 
-             periodCheck = true;
-            } 
-            ParCheck = false; 
-            break;
+    if (typeof input === 'number' && input >= 0 && input <= 9) {
+        // Handle numbers
+        appendToField(input);
+        ParCheck = true;
+    } else if (input === 'P') {
+        // Handle parentheses
+        if (!ParCheck) {
+            appendToField('(');
+            parCount++;
+        } else {
+            appendToField(')');
+            parCount--;
+        }
+    } else if (input === '.') {
+        // Handle decimal point
+        if (!periodCheck) {
+            appendToField('.');
+            periodCheck = true;
+        }
+        ParCheck = false;
     }
     calculation = document.getElementById('enterfield').value ;
+    // saves input field value to be used in different functions (deleting and clearing)
 }
 
 function EnterAction(n){
     Cleanup();
     CheckForPar();
+    
     let calculationfield = document.getElementById('enterfield').value;
     switch (n){
         case 'D': 
@@ -92,49 +57,55 @@ function EnterAction(n){
             }
             del = calculationfield.substring(0, calculationfield.length-1); 
             document.getElementById('enterfield').value = del;
-            calculation =  document.getElementById('enterfield').value
+
             break;
         case 'C':
             document.getElementById('enterfield').value = '';
             periodCheck = false;
             ParCheck = false;
             parCount = 0;
-            calculation = document.getElementById('enterfield').value
             break;
 
     }
+    calculation =  document.getElementById('enterfield').value
 }
 
 function EnterOperator(n){
     Cleanup();
+    let enterfield = document.getElementById('enterfield').value;
     if(n != "="){
-        let calculationfield = document.getElementById('enterfield').value;
-        if(calculationfield.length == 0 && n != "-" || calculationfield == "-"){
+        if(enterfield.length == 0 && n != "-" || enterfield == "-"){
+            // returns if input is any operator except - or if inputfield only consists of a -
             return;
         } else{
-            let lastChar = document.getElementById("enterfield").value.slice(-1);
+            let lastChar = enterfield.value.slice(-1);
+            // getst the last character
             if(lastChar == "+" || lastChar == "-" || lastChar == "/" || lastChar =="*" || lastChar =="%"){
-                let del = calculationfield.substring(0, calculationfield.length-1); 
-                document.getElementById('enterfield').value = del +n;
+
+                let del = enterfield.substring(0, enterfield.length-1); 
+                changeFieldValue(del +n);
+                // removes previous operator and replaces it with the new operator inputted 
+
             }else{
                 if(lastChar == "."){
-                    document.getElementById('enterfield').value += "0" + n;
+                   appendToField("0" + n);
                 } else{
-                    document.getElementById('enterfield').value += n;
+                   appendToField(n);
                 }
                 periodCheck = false;
-            }   
+            }   // Handles period operator reiteration 
         }
     }
     else{
          if(document.getElementById('enterfield').value == ""){
+            // prevents any output if enterfield is empty
             return;
          }else{
             try{
-                document.getElementById('enterfield').value = eval(calculation);
+                changeFieldValue(eval(calculation));
             }
             catch(e){
-                document.getElementById('enterfield').value = "Format Error";
+                changeFieldValue("Format Error");
             }
             periodCheck = false;
             ParCheck = false;
@@ -147,9 +118,23 @@ function CheckForPar(){
     if(parCount == 0){
         ParCheck = false;
     }
+    // checks if there are any parenthesis in the inputfield
 }
 function Cleanup(){
     if(document.getElementById('enterfield').value == "undefined" || document.getElementById('enterfield').value == "Format Error"){
         document.getElementById('enterfield').value ="";
     }
+}
+
+function appendToField(value){
+
+    // Gets current value of the input field
+    const enterField = document.getElementById('enterfield');
+    // appends input to current value 
+    enterField.value += value;
+}
+
+function changeFieldValue(value){
+    const enterField = document.getElementById('enterfield');
+    enterField.value = value;
 }
